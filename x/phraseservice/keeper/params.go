0bb3ed293d66f6c697271ad9e4e410519239bb08
Developer Experience team at Tendermint <hello@tendermint.com>
2020-10-07 18:39:17 +0600

    Initialized with Starport

---
+++ x/phraseservice/keeper/params.go
@@ -0,0 +1,22 @@
+package keeper
+/*
+// TODO: Define if your module needs Parameters, if not this can be deleted
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/danikarik/phraseservice/x/phraseservice/types"
+)
+
+// GetParams returns the total set of phraseservice parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramspace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the phraseservice parameters to the param space.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramspace.SetParamSet(ctx, &params)
+}
+*/
